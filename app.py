723782d617c1a9a3a623a820e3f15680618ec3a8
Gireesh Kumar Gowd <gireeshkumargowd@gmail.com>
2026-06-14 17:46:04 +0530

    commit logging and exception

---
+++ app.py
@@ -0,0 +1,8 @@
+from src.DataScienceProject.logger import logging
+from src.DataScienceProject.exception import CustomException
+import sys
+
+
+if __name__=="__main__":
+    logging.info("Starting the application")
+    
