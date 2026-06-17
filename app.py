a90f9386d3da797fea0df5bf7478fb9c3038e1a4
Gireesh Kumar Gowd <gireeshkumargowd@gmail.com>
2026-06-17 16:29:12 +0530

    Data ingestion phase commit

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from src.DataScienceProject.logger import logging
 from src.DataScienceProject.exception import CustomException
+from src.DataScienceProject.components.data_ingestion import DataIngestion, DataIngestionConfig
 import sys
 
 
 if __name__=="__main__":
-    logging.info("Starting the application")
+    logging.info("The execution has started")
+
+    try:
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+        
     
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
