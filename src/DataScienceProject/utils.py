d6082cf9c338ac441dfccd44a1a20544e7b77b3a
Gireesh Kumar Gowd <gireeshkumargowd@gmail.com>
2026-06-29 11:45:58 +0530

    Data transformation commit

--- src/DataScienceProject/utils.py
+++ src/DataScienceProject/utils.py
@@ -21,20 +21,38 @@ db=os.getenv('db')
 
 def read_sql_data():
     logging.info("Reading SQL database started")
+    mydb = None
     try:
-        mydb=pymysql.connect(
+        mydb = pymysql.connect(
             host=host,
             user=user,
             password=password,
             db=db
         )
-        logging.info("Connection Established",mydb)
-        df=pd.read_sql_query('Select * from students',mydb)
+        logging.info("Connection established to %s/%s", host, db)
+        cursor = mydb.cursor()
+        cursor.execute('Select * from students')
+        data = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        df = pd.DataFrame(data, columns=columns)
         print(df.head())
 
         return df
 
-
-
     except Exception as ex:
         raise CustomException(ex)
+    finally:
+        if mydb is not None:
+            mydb.close()
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
